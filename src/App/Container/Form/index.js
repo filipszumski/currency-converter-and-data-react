17387cdf1867f759802dbce34458e590e8f560b1
Filip Szumski <filipszumski@gmail.com>
2020-09-05 19:27:51 +0200

    added useState variables to form elements and made controlled elements

--- src/App/Container/Form/index.js
+++ src/App/Container/Form/index.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import {currencies} from "../../currencies";
 
 const Form = () => {
+  const [possessedCurrency, setPossessedCurrency] = useState(currencies[0].short);
+  const [amount, setAmount] = useState("");
+  const [wantedCurrency, setWantedCurrency] = useState(currencies[0].short);
+
+  console.log(possessedCurrency);
 
   return (
     <form className="form" action="" method="GET">
       <p className="form__paragraph">
         <label className="form__label">
           <span className="form__labelText">Wymieniasz z (wybierz walutę):</span>
-          <select className="form__input">
+          <select
+            className="form__input"
+            value={possessedCurrency}
+            onChange={event => setPossessedCurrency(event.target.value)}
+          >
             <option name="currency" value="pln">PLN</option>
             <option name="currency" value="eur">EUR</option>
             <option name="currency" value="usd">USD</option>
@@ -21,17 +31,28 @@ const Form = () => {
       <p className="form__paragraph">
         <label className="form__label">
           <span className="form__labelText">Kwota:</span>
-          <input type="number" name="amount" step="0.01" placeholder="Wpisz kwotę"
-            className="form__input" min="0" />
+          <input
+            type="number"
+            name="amount"
+            step="0.01"
+            placeholder="Wpisz kwotę"
+            className="form__input" min="0"
+            value={amount}
+            onChange={event => setAmount(event.target.value)}
+          />
         </label>
       </p>
 
       <p className="form__paragraph">
         <label className="form__label">
           <span className="form__labelText">Wymieniasz na (wybierz walutę):</span>
-          <select className="form__input">
+          <select 
+          className="form__input"
+          value={wantedCurrency}
+          onChange={event => setWantedCurrency(event.target.value)}
+          >
             <option name="currency" value="pln">PLN</option>
-            <option se name="currency" value="eur">EUR</option>
+            <option name="currency" value="eur">EUR</option>
             <option name="currency" value="usd">USD</option>
             <option name="currency" value="gbp">GBP</option>
             <option name="currency" value="chf">CHF</option>
