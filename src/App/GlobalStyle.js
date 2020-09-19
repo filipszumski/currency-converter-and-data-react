52ea4a2c203096858536791098ad1b4bb8db82d9
Filip Szumski <filipszumski@gmail.com>
2020-09-19 20:36:59 +0200

    installed styled components

---
+++ src/App/GlobalStyle.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+import background from "../images/dollar-5360047_1920.jpg"
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+    box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+    box-sizing: inherit;
+    }
+
+    #root {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    background-image: url("${background}");
+    background-size: cover;
+    background-position: center;
+    font-family: 'Montserrat', sans-serif;
+    }
+`;
--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Header } from "./Header";
 import { Form } from "./Form";
-import { Clock } from "./Clock"
-import "./style.css"
+import { Clock } from "./Clock";
+import "./style.css";
+import {GlobalStyle} from "./GlobalStyle";
 
 function App() {
   return (
     <div className="app">
+      <GlobalStyle />
       <Clock />
       <Header />
       <Form />
