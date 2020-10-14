0b0484b874cc695bdc733eaf91125a584dc8de06
Filip Szumski <filipszumski@gmail.com>
2020-10-14 23:12:53 +0200

    changes padding, opacity and box-shadow in container

--- src/App/styled.js
+++ src/App/styled.js
@@ -4,8 +4,9 @@ export const Wrapper = styled.div`
     flex: 0 1 750px;
     font-size: 18px;
     background-color: ${({theme}) => theme.colors.background};
-    padding: 30px;
+    padding: 40px;
     border-radius: 50px;
-    opacity: 0.85;
+    box-shadow: 0px 0px 8px 1px;
+    opacity: 0.90;
     margin: 20px;
 `;
