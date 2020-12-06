9a6375e836a96a53161ce96f65ac92f0b5761c3b
Filip Szumski <filipszumski@gmail.com>
2020-12-06 23:47:23 +0100

    navigation styling done

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,16 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
+import { Nav, List, Item, StyledLink } from "./styled";
 import { toConverter } from "../../routes";
 
 export const Navigation = () => (
-    <nav>
-        <nav>
-            <ul>
-                <li>
-                    <Link to={toConverter()}>Kalkulator Walut</Link>
-                </li>
-            </ul>
-        </nav>
-    </nav>
+    <Nav>
+        <List>
+            <Item>
+                <StyledLink to={toConverter()}>Kalkulator Walut</StyledLink>
+            </Item>
+            <Item>
+                <StyledLink to={toConverter()}>Kalkulator Walut</StyledLink>
+            </Item>
+        </List>
+    </Nav>
+
 
 )
---
+++ src/common/Navigation/styled.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Nav = styled.nav`
+flex: 1 0 100%;
+background-color: ${({ theme }) => theme.colors.buttonBackground};
+`;
+
+export const List = styled.ul`
+display: flex;
+justify-content: center;
+margin: 0;
+padding: 0;
+list-style-type: none;
+`;
+
+export const Item = styled.li`
+margin: 20px 10px 20px 10px;
+`;
+
+export const StyledLink = styled(Link)`
+color: ${({ theme }) => theme.colors.background};
+text-decoration: none;
+`;
