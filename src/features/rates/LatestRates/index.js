60ad0058e598fcac3b6bf180d0182b34671045b6
Filip Szumski <filipszumski@gmail.com>
2021-01-04 01:47:03 +0100

    added component and styled-components with latest rates table

--- src/features/rates/LatestRates/index.js
+++ src/features/rates/LatestRates/index.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Container } from "../../../common/Container";
 import { Header } from "../../../common/Header";
+import { BaseInput } from "../BaseInput";
+import { Symbol, Table, TableCell, TableHeader, TableRow, TableContainer } from "./styled";
+import { getRates, selectBase, selectLatestDayRates, selectPreviousToLatestDayRates } from "../ratesSlice";
+
 const LatestRates = () => {
+  const dispatch = useDispatch();
+
+  const rates = useSelector(selectLatestDayRates);
+  const base = useSelector(selectBase);
+  const previousDayRates = useSelector(selectPreviousToLatestDayRates);
+
+  useEffect(() => {
+    dispatch(getRates());
+  }, [dispatch, base]);
 
   return (
     <Container>
       <Header title="Ostatnie stawki" />
-      LATEST RATES
+      <TableContainer>
+        <Table>
+          <tbody>
+            <TableRow>
+              <TableHeader scope="col">Currency</TableHeader>
+              <TableHeader scope="col">Spot</TableHeader>
+              <TableHeader scope="col">Increace/Decrease</TableHeader>
+              <TableHeader scope="col">Chart</TableHeader>
+            </TableRow>
+            {(rates !== undefined && rates.length > 0) && rates.map((rate, index) => (
+              <TableRow key={rate[0]}>
+                <TableCell>{rate[0]}</TableCell>
+                <TableCell>{rate[1].toFixed(4)}</TableCell>
+                <TableCell>
+                  {(rate[1] / previousDayRates[index][1]) > 1 ?
+                    <Symbol increase>	&#129033;</Symbol> :
+                    <Symbol decrease>	&#129035;</Symbol>}
+                </TableCell>
+                <TableCell><span role="img">📉</span></TableCell>
+              </TableRow>
+            ))}
+          </tbody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 };
--- src/features/rates/LatestRates/styled.js
+++ src/features/rates/LatestRates/styled.js
@@ -1,83 +1,44 @@
 import styled, { css } from "styled-components";
 
-export const StyledForm = styled.form`
-    display: grid;
-    grid-auto-columns: 1fr;
-    grid-gap: 20px;
+export const Symbol = styled.span`
+    font-size: 25px;
+
+    ${({ increase }) => increase && css`
+        color: ${({ theme }) => theme.colors.increase};
+    `};
+    ${({ decrease }) => decrease && css`
+        color: ${({ theme }) => theme.colors.decrease};
+    `};
+
 `;
 
-export const Paragraph = styled.p`
-    display: flex;
-    margin: 0px;
-
-    ${({ info }) => info && css`
-        color: ${({ theme }) => theme.colors.info};
-        font-size: 14px;
-        text-align: center;
-        justify-content: center;
-    `}
-
-    ${({ loadingState }) => loadingState && css`
-        text-align: center;
-        display: inline;
-        color: ${({ theme }) => theme.colors.info};
-        font-size: 18px;
-    `}
-
-    ${({ errorState }) => errorState && css`
-        text-align: center;
-        display: inline;
-        color: ${({ theme }) => theme.colors.error};
-        font-size: 18px;
-    `}
-    
-    ${({ styledResult }) => styledResult && css`
-        display: flex;
-        justify-content: center;
-        flex-wrap: wrap;
-        font-size: 28px;
-        color: ${({ theme }) => theme.colors.result};
-    `}
+export const TableContainer = styled.div`
+    overflow-x: auto;
 `;
 
-export const Label = styled.label`
-    flex: 1 0 100%;
-    display: flex;
-    align-items: center;
-
-    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-            flex-wrap: wrap;
-        }
-    `;
-
-export const Input = styled.input`
-    flex: 0 1 100%;
-    border-radius: 10px;
-    border: 1px solid teal;
-    padding: 8px;
+export const Table = styled.table`
+    border-collapse: collapse;
+    width: 100%;
 `;
 
-export const Button = styled.button`
-    flex: 1;
+export const TableCell = styled.td`
+    border: 1px solid ${({ theme }) => theme.colors.black};
+    padding: 10px;
+    text-align: center;
+`;
+
+export const TableHeader = styled.th`
+    border: 1px solid ${({ theme }) => theme.colors.black};
+    padding: 10px;
     background-color: ${({ theme }) => theme.colors.buttonBackground};
-    border-radius: 5px;
-    color: ${({ theme }) => theme.colors.buttonText};
-    padding: 8px;
-    border: none;
-    transition: filter 0.5s;
-
-    &:hover {
-        filter: brightness(110%);
-    }
-    &:active {
-        filter: brightness(120%);
-    }
+    color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Span = styled.span`
-    flex: 0 0 300px;
-
-    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-        flex: 0 1 auto;
-    }
+export const TableRow = styled.tr`
+&:nth-child(even) {
+    background-color: #eee;
+};
+&:hover {
+    background-color: hsl(180deg 100% 25% / 50%);
+}
 `;
