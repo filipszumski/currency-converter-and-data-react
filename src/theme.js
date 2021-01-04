cac938ce3965b2e5d0439a7fa7ef38857507e120
Filip Szumski <filipszumski@gmail.com>
2021-01-04 01:38:45 +0100

    added new colors to theme

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,15 @@
 export const theme = {
     colors: {
         background: "white",
+        black: "black",
         buttonBackground: "teal",
         buttonText: "white",
         title: "teal",
         info: "rgb(107, 107, 107)",
         result: "teal",
         error: "crimson",
+        increase: "green",
+        decrease: "red",
     },
     breakpoints: {
         mobile: 767,
