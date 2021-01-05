eb3f7203dd5f7b452420fac435aed4924551c283
Filip Szumski <filipszumski@gmail.com>
2021-01-05 21:57:01 +0100

    new Table common component

---
+++ src/common/Table/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyledTable, Wrapper } from "./styled";
+
+export const Table = ({ body }) => {
+
+    return (
+        <Wrapper>
+            <StyledTable>
+                <tbody>
+                    {body}
+                </tbody>
+            </StyledTable>
+        </Wrapper>
+    )
+}
---
+++ src/common/Table/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    overflow-x: auto;
+`;
+
+export const StyledTable = styled.table`
+    border-collapse: collapse;
+    width: 100%;
+`;
