15d0db19cf7a6c98a5994175309034f8a9f07e79
Filip Szumski <filipszumski@gmail.com>
2021-01-05 21:59:47 +0100

    added Button styled component to styled.js file in features/rates folder

--- src/features/rates/styled.js
+++ src/features/rates/styled.js
@@ -6,3 +6,20 @@ export const Input = styled.input`
     border: 1px solid teal;
     padding: 5px;
 `;
+
+export const Button = styled.button`
+    flex: 1;
+    background-color: ${({ theme }) => theme.colors.buttonBackground};
+    border-radius: 5px;
+    color: ${({ theme }) => theme.colors.buttonText};
+    padding: 8px;
+    border: none;
+    transition: filter 0.5s;
+
+    &:hover {
+        filter: brightness(110%);
+    }
+    &:active {
+        filter: brightness(120%);
+    }
+`;
