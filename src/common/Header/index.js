2c5d0397fec6a48bf12144551ebc351335313711
Filip Szumski <filipszumski@gmail.com>
2021-01-06 20:25:46 +0100

    chaged Header component to display base rate input in chosen components

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -3,15 +3,32 @@ import { Container } from "./styled";
 import { Clock } from "./Clock";
 import { BaseInput } from "../../features/rates/BaseInput";
 import { StyledHeader } from "./styled";
+import { useLocation, useParams } from "react-router-dom";
 
-const Header = ({ title }) => (
-    <Container>
-        <BaseInput />
-        <Clock />
-        <StyledHeader>
-            {title}
-        </StyledHeader>
-    </Container>
-);
+const Header = ({ title }) => {
+    const location = useLocation();
+    const params = useParams();
+
+    return (
+        <Container>
+            {location.pathname === "/convarter" ?
+                <></> :
+                location.pathname === "/latest-rates" ?
+                    <BaseInput /> :
+                    location.pathname === `/latest-rates/${params.id}` ?
+                        <></> :
+                        location.pathname === "/chart" ?
+                            <BaseInput /> :
+                            location.pathname === "/trends" ?
+                                <BaseInput /> :
+                                <></>
+            }
+            <Clock />
+            <StyledHeader>
+                {title}
+            </StyledHeader>
+        </Container>
+    )
+};
 
 export { Header };
